0879482efd7c07afb38d9aea8e8ddddd3e124aac
Siddharth Thakor <sidht1295@gmail.com>
2019-05-10 00:58:20 -0400

    updates: Logo, Navigation added

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Auxi from '../../../hoc/Auxi'
+import Auxi from '../../../hoc/Auxi/Auxi'
+import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    // functional component
+    componentWillUpdate() {
+        console.log('[orderSummary] will update')
+    }
+
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li> );
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>);
         });
 
-    return (
-    <Auxi>
-        <h3>Your Order</h3>
-        <p>A delicious buger with the following ingredients:</p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-        <p>Continue to Checkout?</p>
-    </Auxi>
-    );
-};
+        return (
+            <Auxi>
+                <h3>Your Order</h3>
+                <p>A delicious buger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Auxi>
+        );
+    }
+} 
 
-export default orderSummary;
+export default OrderSummary;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Aux from '../../hoc/Auxi'
+import Aux from '../../hoc/Auxi/Auxi'
 import Burger from '../../components/Burger/Burger'
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
@@ -74,6 +74,11 @@ class BurgerBuilder extends Component {
     purchaseCancelHandler = () => {
         this.setState({purchasing: false});
     }
+
+    purchaseContinueHandler = () => {
+        alert('You Continue');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -84,7 +89,11 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        ingredients={this.state.ingredients} 
+                        price={this.state.totalPrice}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
